8d4279973af53dbbf09c9dde358f4310faaa9be6
RobertoAguirre <roberto.aguirre.rodriguez@gmail.com>
2022-08-01 09:25:44 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { CallbackID, Capacitor } from '@capacitor/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  coordinate: any;
+  watchCoordinate: any;
+  watchId: any;
+  constructor(private zone: NgZone) {}
+
+  async requestPermissions() {
+    const permResult = await Geolocation.requestPermissions();
+    console.log('Perm request result: ', permResult);
+  }
+
+  getCurrentCoordinate() {
+    if (!Capacitor.isPluginAvailable('Geolocation')) {
+      console.log('Plugin geolocation not available');
+      return;
+    }
+    Geolocation.getCurrentPosition().then(data => {
+      this.coordinate = {
+        latitude: data.coords.latitude,
+        longitude: data.coords.longitude,
+        accuracy: data.coords.accuracy
+      };
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+
+  watchPosition() {
+    try {
+      this.watchId = Geolocation.watchPosition({}, (position, err) => {
+        console.log('Watch', position);
+        this.zone.run(() => {
+          this.watchCoordinate = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
+        });
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  clearWatch() {
+    if (this.watchId != null) {
+      Geolocation.clearWatch({ id: this.watchId });
+    }
+  }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  public url = 'localhost:42000'
+
+  constructor() { }
+}
