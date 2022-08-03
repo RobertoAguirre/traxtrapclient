72829f41bc5b29dcd56faf1083552c7d78c227ff
RobertoAguirre <roberto.aguirre.rodriguez@gmail.com>
2022-08-03 12:09:31 -0600

    refactored to home from appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component, NgZone, OnInit } from '@angular/core';
 import { CallbackID, Capacitor } from '@capacitor/core';
 import { Geolocation } from '@capacitor/geolocation';
 
-import { Socket } from 'ngx-socket-io';
-import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -11,97 +9,19 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  coordinate: any;
-  watchCoordinate: any;
-  watchId: any;
+
 
   message = '';
   messages = [];
   currentUser = '';
 
-  constructor(private zone: NgZone, private socket: Socket, private toastCtrl: ToastController) { }
+  constructor() { }
 
   ngOnInit() {
-    this.socket.connect();
-
-    let name = `user-${new Date().getTime()}`;
-    this.currentUser = name;
-
-    this.socket.emit('set-name', name);
-
-    this.socket.fromEvent('users-changed').subscribe(data => {
-      let user = data['user'];
-      if (data['event'] === 'left') {
-        this.showToast('User left: ' + user);
-      } else {
-        this.showToast('User joined: ' + user);
-      }
-    });
-
-    this.socket.fromEvent('message').subscribe(message => {
-      this.messages.push(message);
-    });
+  
   }
 
-  sendMessage() {
-    this.socket.emit('send-message', { text: this.message });
-    this.message = '';
-  }
-  ionViewWillLeave() {
-    this.socket.disconnect();
-  }
 
-  async showToast(msg) {
-    let toast = await this.toastCtrl.create({
-      message: msg,
-      position: 'top',
-      duration: 2000
-    });
-    toast.present();
-  }
 
-  ///geolocation
-
-  async requestPermissions() {
-    const permResult = await Geolocation.requestPermissions();
-    console.log('Perm request result: ', permResult);
-  }
-
-  getCurrentCoordinate() {
-    if (!Capacitor.isPluginAvailable('Geolocation')) {
-      console.log('Plugin geolocation not available');
-      return;
-    }
-    Geolocation.getCurrentPosition().then(data => {
-      this.coordinate = {
-        latitude: data.coords.latitude,
-        longitude: data.coords.longitude,
-        accuracy: data.coords.accuracy
-      };
-    }).catch(err => {
-      console.error(err);
-    });
-  }
-
-  watchPosition() {
-    try {
-      this.watchId = Geolocation.watchPosition({}, (position, err) => {
-        console.log('Watch', position);
-        this.zone.run(() => {
-          this.watchCoordinate = {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          };
-        });
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  clearWatch() {
-    if (this.watchId != null) {
-      Geolocation.clearWatch({ id: this.watchId });
-    }
-  }
+ 
 }
